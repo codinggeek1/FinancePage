fb6c52b62875cfe27488fe4f7cdded4d5359b3dd
PARDHIV VARMA <gpardhivvarma.ai21@rvce.edu.in>
2023-11-14 20:12:04 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import './App.css'; // You can import your CSS file here
+import logoImage from '/Users/pardhivvarma/Documents/my-react-app/src/logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+     <header>
+        <div id="logo">
+          <img src={logoImage} alt="Logo" />
+        </div>
+        <button id="connect-button-top">Connect Wallet</button>
       </header>
-    </div>
+
+      <div id="content">
+        <p className="keep-assets-liquid">Keep your assets liquid</p>
+        <div className="text-box">
+          <input type="text" placeholder="Usdc amount MAX" />
+          <button className="connect-button-inside-box">Connect Wallet</button>
+        </div>
+        <p className="custom-text">You will receive crUSDC</p>
+        <p className="exchange-rate">Exchange rate: </p>
+        <p className="conversion">1 crUSDC = 1 USDC</p>
+        <button className="view-etherscan">View on Etherscan</button>
+        <button className="create-transaction">Create Transaction</button>
+      </div>
+    </>
   );
 }
 

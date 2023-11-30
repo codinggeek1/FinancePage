29c9f0729cca1852bdc49bb7f4b1adb88cdf8b3c
PARDHIV VARMA <118751512+codinggeek1@users.noreply.github.com>
2023-11-30 11:13:09 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,14 +5,12 @@ import './Home.css';
 export default function Header({connectWallet}) {
     return (
         <>
-          <header>
             <div id="logo">
               <img src={logoImage} alt="Logo" />
             </div>
             <button id="connect-button-top" onClick={connectWallet}>
               Connect Wallet
             </button>
-          </header>
         </>
     )
 }
